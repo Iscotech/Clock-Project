44ea41285a52d16c0521f942a87aba36b58abd8e
Isaac <iscotech594@gmal.com>
2024-08-14 14:22:42 +0100

    My first Clock Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DigitalClock from "./DigitalClock";
+
+const App = () => {
+  return (
+    <div className="clock-con">
+      <DigitalClock />
+    </div>
+  );
+};
+
+export default App;
